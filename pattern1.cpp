1729e7bc01836302b6b7206da5b323a5b2835e72
paragaher15 <140876887+paragaher15@users.noreply.github.com>
2023-08-15 11:16:08 +0530

    Update pattern1.cpp

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,8 +1,12 @@
-#include<iostream> 
-using namespace std ;
-
-int  main(){
-
-  return 0;
-
+#include<iostream>
+using namespace std;
+int main(){
+    int col = 9;
+    int row = 9;
+    for(int row=0;row<col+1;row++){
+        for(int col=0;col<row;col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 }
